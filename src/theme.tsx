54bf52c2edb5d3d509a49f7eb807d1492e022bca
Low Siew Kam Honey Nicole <winston_lim1@hotmail.com>
2021-07-13 19:26:56 +0800

    Add color mode config to theme

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,21 +1,25 @@
-import { extendTheme } from '@chakra-ui/react'
-import { createBreakpoints } from '@chakra-ui/theme-tools'
+import { extendTheme, ThemeConfig } from "@chakra-ui/react";
+import { createBreakpoints } from "@chakra-ui/theme-tools";
 
-const fonts = { mono: `'Menlo', monospace` }
+const fonts = { mono: `'Menlo', monospace` };
 
 const breakpoints = createBreakpoints({
-  sm: '40em',
-  md: '52em',
-  lg: '64em',
-  xl: '80em',
-})
+	sm: "40em",
+	md: "52em",
+	lg: "64em",
+	xl: "80em",
+});
 
 const theme = extendTheme({
-  colors: {
-    black: '#16161D',
-  },
-  fonts,
-  breakpoints,
-})
+	colors: {
+		black: "#16161D",
+	},
+	fonts,
+	breakpoints,
+	config: {
+		initialColorMode: "light",
+		useSystemColorMode: false,
+	},
+});
 
-export default theme
+export default theme;
