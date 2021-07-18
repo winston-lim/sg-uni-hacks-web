e5bc2c1321f9c71c2669b34179acae753e994bd3
Low Siew Kam Honey Nicole <winston_lim1@hotmail.com>
2021-07-18 19:29:01 +0800

    Update Main component

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -1,4 +1,12 @@
-import { Box, Flex, Spacer, Text } from "@chakra-ui/react";
+import {
+	BackgroundProps,
+	Box,
+	ColorProps,
+	Flex,
+	Spacer,
+	Text,
+	useColorMode,
+} from "@chakra-ui/react";
 import { ReactJSXElement } from "@emotion/react/types/jsx-namespace";
 import React from "react";
 import { Footer } from "./Footer";
@@ -9,15 +17,22 @@ interface MainProps {
 
 export const Main: React.FC<MainProps> = (props: any) => {
 	const { children, footer, ...remainingProps } = props;
+	const { colorMode } = useColorMode();
+	const bgColor = { dark: "gray.900", light: "gray.50" };
+	const color = { dark: "white", light: "black" };
 	return (
 		<Flex
-			width="100%"
+			margin={0}
 			direction="column"
-			height="100vh"
-			justifyContent="space-between"
+			justify="space-between"
+			bg={bgColor[colorMode]}
+			height="100%"
+			color={color[colorMode]}
 			{...remainingProps}
 		>
-			<Box px="6">{children}</Box>
+			<Box px={5} mx="auto">
+				{children}
+			</Box>
 			<Footer>{footer}</Footer>
 		</Flex>
 	);
