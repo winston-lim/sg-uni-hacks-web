0d52bdfe30d41034a04c9c695abf6d49a7785a2f
Low Siew Kam Honey Nicole <winston_lim1@hotmail.com>
2021-07-18 19:26:19 +0800

    Updated DrawerItem styling

--- src/components/data-display/DrawerItem.tsx
+++ src/components/data-display/DrawerItem.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import NextLink from "next/link";
+import { Heading, Link, useColorMode } from "@chakra-ui/react";
 interface DrawerItemProps {
 	title: string;
 	href: string;
 }
 
 export const DrawerItem: React.FC<DrawerItemProps> = ({ title, href }) => {
-	return <NextLink href={href}>{title}</NextLink>;
+	const { colorMode } = useColorMode();
+
+	const color = { dark: "teal.200", light: "blue.200" };
+	return (
+		<NextLink href={href}>
+			<Link fontSize="xxl" color={color[colorMode]} as={Heading}>
+				{title}
+			</Link>
+		</NextLink>
+	);
 };
