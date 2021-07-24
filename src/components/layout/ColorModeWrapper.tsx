f71b02ea732c5b1fd3a75a43317621274916f9fa
Low Siew Kam Honey Nicole <winston_lim1@hotmail.com>
2021-07-24 18:48:06 +0800

    Update styles for ColorMode wrapper

--- src/components/layout/ColorModeWrapper.tsx
+++ src/components/layout/ColorModeWrapper.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Container } from "./Container";
 import { DarkModeSwitch } from "../icons/DarkModeSwitch";
+import { Box, Flex } from "@chakra-ui/react";
+import { Footer } from "./Footer";
 
 export const ColorModeWrapper: React.FC<{}> = ({ children }) => {
 	return (
-		<Container height="100%">
-			{children}
-			<DarkModeSwitch />
-		</Container>
+		<Flex margin={0} direction="column" justify="space-between">
+			<Box px={5} mx="auto">
+				{children}
+			</Box>
+			<DarkModeSwitch isFixed={true} />
+		</Flex>
 	);
 };
