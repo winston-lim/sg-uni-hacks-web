9a1566c6a8e8607726e2a0c2ff2a0ba7257bcae7
Low Siew Kam Honey Nicole <winston_lim1@hotmail.com>
2021-07-26 15:39:27 +0800

    Update Container styles

--- src/components/layout/Container.tsx
+++ src/components/layout/Container.tsx
@@ -3,11 +3,13 @@ import { Box, useColorMode } from "@chakra-ui/react";
 export const Container = (props: any) => {
 	const { colorMode } = useColorMode();
 
-	const bgColor = { light: "gray.900", dark: "gray.50" };
+	const bgColor = { dark: "gray.900", light: "gray.50" };
 
-	const color = { light: "white", dark: "black" };
+	const color = { dark: "white", light: "black" };
 	return (
 		<Box
+			minH="100vw"
+			maxW="100vw"
 			alignItems="center"
 			bg={bgColor[colorMode]}
 			color={color[colorMode]}
