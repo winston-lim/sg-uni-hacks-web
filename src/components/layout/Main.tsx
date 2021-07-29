6d8e534716084b037a054b4db2b56f1bd76ba442
Low Siew Kam Honey Nicole <winston_lim1@hotmail.com>
2021-07-29 16:22:01 +0800

    Update Main component to have dynamic height

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -8,7 +8,8 @@ import {
 	useColorMode,
 } from "@chakra-ui/react";
 import { ReactJSXElement } from "@emotion/react/types/jsx-namespace";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Filler } from "./Filler";
 import { Footer } from "./Footer";
 
 interface MainProps {
@@ -17,9 +18,13 @@ interface MainProps {
 
 export const Main: React.FC<MainProps> = (props: any) => {
 	const { children, footer, ...remainingProps } = props;
+	const [mainContentId, setMainContentId] = useState<string | null>(null);
 	const { colorMode } = useColorMode();
 	const bgColor = { dark: "gray.900", light: "gray.50" };
 	const color = { dark: "white", light: "black" };
+	useEffect(() => {
+		setMainContentId("mainContentContainer");
+	}, []);
 	return (
 		<Flex
 			margin={0}
@@ -31,9 +36,10 @@ export const Main: React.FC<MainProps> = (props: any) => {
 			maxW="100vw"
 			{...remainingProps}
 		>
-			<Box px={5} mx="auto">
+			<Box id="mainContentContainer" mb={10} px={5} mx="auto">
 				{children}
 			</Box>
+			{mainContentId ? <Filler relativeContainerId={mainContentId} /> : null}
 			<Footer>{footer}</Footer>
 		</Flex>
 	);
