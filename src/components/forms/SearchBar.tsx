841dd1bb12221159705152dd4c499e39e44876ce
Low Siew Kam Honey Nicole <winston_lim1@hotmail.com>
2021-08-04 16:55:50 +0800

    Update search bar functionality

--- src/components/forms/SearchBar.tsx
+++ src/components/forms/SearchBar.tsx
@@ -1,16 +1,27 @@
 import { Input, useColorMode } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import useDebounce from "../../utils/useDebounce";
 
 interface SearchBarProps {
 	display?: any;
+	setSearchTerm: React.Dispatch<React.SetStateAction<string>>;
 }
 
-export const SearchBar: React.FC<SearchBarProps> = ({ display }) => {
+export const SearchBar: React.FC<SearchBarProps> = ({
+	display,
+	setSearchTerm,
+}) => {
 	const { colorMode } = useColorMode();
-	console.log(colorMode);
-
+	const [value, setValue] = useState<string>("");
+	const debouncedValue = useDebounce<string>(value, 2000);
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setValue(event.target.value);
+	};
+	useEffect(() => {
+		setSearchTerm(debouncedValue);
+	}, [debouncedValue]);
 	const bgColor = { dark: "gray.700", light: "gray.50" };
-
 	const color = { dark: "white", light: "black" };
 	return (
 		<Input
@@ -21,6 +32,8 @@ export const SearchBar: React.FC<SearchBarProps> = ({ display }) => {
 			w={{ md: "500px", lg: "800px" }}
 			mr="5"
 			display={display}
+			value={value}
+			onChange={handleChange}
 		/>
 	);
 };
