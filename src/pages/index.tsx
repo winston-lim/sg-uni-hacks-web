a0bc2a6cf4ee80a26a5f853d358dbee320a3da19
Low Siew Kam Honey Nicole <winston_lim1@hotmail.com>
2021-08-05 19:02:43 +0800

    Update index page to support searching

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,51 +1,128 @@
-import {
-	Link as ChakraLink,
-	Text,
-	Code,
-	List,
-	ListIcon,
-	ListItem,
-	Box,
-	Spacer,
-} from "@chakra-ui/react";
-import { CheckCircleIcon, LinkIcon } from "@chakra-ui/icons";
+import { Button, Flex, Heading, useColorMode } from "@chakra-ui/react";
 import { Main } from "../components/layout/Main";
-import React from "react";
-import { ResponsiveWrapper } from "../components/layout/ResponsiveWrapper";
+import React, { useEffect, useState } from "react";
 import { SizedBox } from "../components/layout/SizedBox";
-import { SizedHeading } from "../components/typography/SizedHeading";
 import { Header } from "../components/layout/Header";
 import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import { BasicFooter } from "../components/layout/BasicFooter";
+import { SizedWrapper } from "../components/layout/SizedWrapper";
+import { useRouter } from "next/router";
+import { FractionContainer } from "../components/layout/FractionContainer";
+import { useVerifiedHacksBySearchTermQuery } from "../generated/graphql";
+import { HackItem } from "../components/data-display/HackItem";
+import { ColorConfig, fallbackBackgroundUrl } from "../types/default";
+import { MainItem } from "../components/data-display/MainItem";
+
 const Index = () => {
+	const [variables, setVariables] = useState({
+		limit: 4,
+		cursor: null as null | string,
+		searchTerm: "",
+	});
+	const [searchTerm, setSearchTerm] = useState<string>("");
+	useEffect(() => {
+		setVariables({
+			limit: variables.limit,
+			cursor: variables.cursor,
+			searchTerm,
+		});
+	}, [searchTerm]);
+
+	const [{ data, fetching }] = useVerifiedHacksBySearchTermQuery({
+		variables,
+	});
+
+	const router = useRouter();
+	const { colorMode } = useColorMode();
+	const colorConfig: ColorConfig = {
+		colorMode: colorMode,
+		colorScheme: { dark: "blue", light: "teal" },
+		errorColor: { dark: "pink", light: "red" },
+		color: { dark: "white", light: "black" },
+		bgColor: { dark: "blue.200", light: "teal.200" },
+		accentColor: { light: "white", dark: "#000d1a" },
+	};
+	let articleBlock = null;
+	if (fetching) {
+		articleBlock = (
+			<Heading fontSize={{ base: "xl", md: "2xl", lg: "4xl" }}>Loading</Heading>
+		);
+	} else if (!fetching && data?.verifiedHacksBySearchTerm.hacks.length === 0) {
+		console.log("no data");
+		articleBlock = (
+			<Heading fontSize={{ base: "xl", md: "2xl", lg: "4xl" }}>
+				No posts available now
+			</Heading>
+		);
+	} else {
+		console.log("data", data);
+		let hacks = data!.verifiedHacksBySearchTerm.hacks;
+		const dataItems = hacks!.map((hack) => {
+			const links = hack.s3Url ? JSON.parse(hack.s3Url) : undefined;
+			const hasCoverImage = !!links?.otherLinks;
+			return (
+				<HackItem
+					creator={hack.creator.username}
+					router={router}
+					id={hack.id}
+					title={hack.title}
+					category={hack.category}
+					body={hack.body}
+					colorConfig={colorConfig}
+					descriptionSnippet={hack.descriptionSnippet}
+					duration={hack.duration}
+					likes={hack.points}
+					updatedAt={hack.updatedAt}
+					coverPhoto={hasCoverImage ? links.otherLinks : fallbackBackgroundUrl}
+					voteStatus={hack.voteStatus!}
+					key={hack.id}
+					isLast={hacks![hacks!.length - 1] === hack}
+				/>
+			);
+		});
+		articleBlock = (
+			<Flex align="center" mb={10} direction="column">
+				{dataItems}
+				{data && data.verifiedHacksBySearchTerm.hasMore ? (
+					<Button
+						onClick={() => {
+							setVariables({
+								limit: variables.limit,
+								cursor:
+									data.verifiedHacksBySearchTerm.hacks[
+										data.verifiedHacksBySearchTerm.hacks.length - 1
+									].updatedAt,
+								searchTerm: variables.searchTerm,
+							});
+						}}
+						isLoading={fetching}
+						m="auto"
+						my={8}
+					>
+						load more...
+					</Button>
+				) : null}
+			</Flex>
+		);
+	}
 	return (
 		<>
-			<Header headerVariant="searchbar" />
+			<Header setSearchTerm={setSearchTerm} headerVariant="searchbar" />
 			<Main footer={<BasicFooter />}>
-				<SizedBox height={20} />
-				<ResponsiveWrapper>
-					<SizedHeading variant="xl" title="Sign in" />
-					<SizedBox height={15} />
-					<Text>
-						Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code>.
-					</Text>
-					<List spacing={3} my={0}>
-						<ListItem>
-							<ListIcon as={CheckCircleIcon} color="green.500" />
-							<ChakraLink href="https://chakra-ui.com" flexGrow={1} mr={2}>
-								Chakra UI <LinkIcon />
-							</ChakraLink>
-						</ListItem>
-						<ListItem>
-							<ListIcon as={CheckCircleIcon} color="green.500" />
-							<ChakraLink href="https://nextjs.org" flexGrow={1} mr={2}>
-								Next.js <LinkIcon />
-							</ChakraLink>
-						</ListItem>
-					</List>
-				</ResponsiveWrapper>
-				<Box h={{ base: 200, sm: 300, md: 450 }} bgColor="grey.800" />
+				<SizedBox height={5} />
+				<SizedWrapper
+					width={
+						{ base: "100vw", sm: "100vw", md: "800px", lg: "1200px" } as any
+					}
+				>
+					<FractionContainer
+						col1={7}
+						col2={3}
+						child1={articleBlock!}
+						child2={<MainItem colorConfig={colorConfig} />}
+					/>
+				</SizedWrapper>
 			</Main>
 		</>
 	);
