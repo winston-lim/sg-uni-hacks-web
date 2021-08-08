4332733a26603b7e2693c656f40825416cdc1668
Low Siew Kam Honey Nicole <winston_lim1@hotmail.com>
2021-08-08 14:46:58 +0800

    Update _app to use react-notion-x styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
 import "../styles.css";
 import theme from "../theme";
+import "react-notion-x/src/styles.css";
 
 function MyApp({ Component, pageProps }: any) {
 	return (
