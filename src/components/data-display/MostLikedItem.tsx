0cea5dac2b90d393ce5d7f33f0b08542e82d13f7
Low Siew Kam Honey Nicole <winston_lim1@hotmail.com>
2021-08-08 14:45:47 +0800

    Add MostLikedItem component

---
+++ src/components/data-display/MostLikedItem.tsx
@@ -0,0 +1,53 @@
+import { HStack, Stack, Text, Image, Link, Flex } from "@chakra-ui/react";
+import React from "react";
+import { ColorConfig } from "../../types/default";
+
+interface MostLikedItemProps {
+	id: string;
+	title: string;
+	category: string;
+	updatedAt: string;
+	duration: number;
+	creatorName: string;
+	colorConfig: ColorConfig;
+	coverImage: string;
+}
+
+export const MostLikedItem: React.FC<MostLikedItemProps> = ({
+	id,
+	title,
+	category,
+	updatedAt,
+	duration,
+	creatorName,
+	colorConfig,
+	coverImage,
+}) => {
+	const colorMode = colorConfig.colorMode;
+	const date = new Date();
+	date.setTime(parseInt(updatedAt));
+	return (
+		<HStack align="flex-start" w="100%" spacing={5}>
+			<Image boxSize="100" src={coverImage}></Image>
+			<Stack w="100%" color={colorConfig.color[colorMode]} spacing={1}>
+				<Text
+					fontSize="sm"
+					textColor={colorMode === "dark" ? "whiteAlpha.600" : "blackAlpha.600"}
+				>
+					<b>{creatorName}</b> in <b>{category}</b>
+				</Text>
+				<Text fontSize="lg">
+					<b>{title}</b>
+				</Text>
+				<Text fontSize="md">
+					{date.toLocaleDateString()} - {duration} min read
+				</Text>
+				<Flex justify="flex-end" w="100%">
+					<Link color="green.500" href={`/posts/${id}`}>
+						<u>View</u>
+					</Link>
+				</Flex>
+			</Stack>
+		</HStack>
+	);
+};
