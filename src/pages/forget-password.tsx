7f8774869ac23c149abd54018034bca303e2e020
Low Siew Kam Honey Nicole <winston_lim1@hotmail.com>
2021-08-11 00:16:11 +0800

    Add form validation and update styling for forget-password page

--- src/pages/forget-password.tsx
+++ src/pages/forget-password.tsx
@@ -5,23 +5,16 @@ import {
 	Box,
 	Button,
 	Flex,
-	HStack,
-	Link,
 	Text,
 	useColorMode,
 } from "@chakra-ui/react";
 import { Form, Formik } from "formik";
 import React, { useState } from "react";
 import { useRouter } from "next/router";
-import { toErrorMap } from "../utils/toErrorMap";
 import { InputField } from "../components/forms/InputField";
-import {
-	useForgotPasswordMutation,
-	useLoginMutation,
-} from "../generated/graphql";
+import { useForgotPasswordMutation } from "../generated/graphql";
 import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../utils/createUrqlClient";
-import NextLink from "next/link";
 import { ResponsiveWrapper } from "../components/layout/ResponsiveWrapper";
 import { SizedHeading } from "../components/typography/SizedHeading";
 import { SizedBox } from "../components/layout/SizedBox";
@@ -29,11 +22,11 @@ import { Main } from "../components/layout/Main";
 import { BasicFooter } from "../components/layout/BasicFooter";
 import { DarkModeSwitch } from "../components/icons/DarkModeSwitch";
 import { ColorConfig } from "../types/default";
-import login from "./login";
 import {
 	handleGraphqlError,
 	handleGraphqlSuccess,
 } from "../utils/handleGraphqlResponse";
+import { ForgetPasswordValidationSchema } from "../utils/validationSchemas";
 
 export const ForgetPassword: React.FC<{}> = ({}) => {
 	const router = useRouter();
@@ -54,65 +47,68 @@ export const ForgetPassword: React.FC<{}> = ({}) => {
 			<Main footer={<BasicFooter />}>
 				<SizedBox height={20} />
 				<ResponsiveWrapper>
-					<SizedHeading variant="xl" title="Forgot password" />
-					<Box h={5} />
-					<Text fontSize="lg">
-						enter your email to request for a password change
-					</Text>
-					{alert === "" ? null : (
-						<Flex my={4} w="100%" justifyContent="center">
-							<Alert
-								maxW={{ base: "450px", md: "600px" }}
-								status={
-									alert.split(":")[0].includes("error") ? "error" : "success"
-								}
-							>
-								<AlertIcon />
-								<AlertTitle mr={2}>{alert}</AlertTitle>
-							</Alert>
-						</Flex>
-					)}
-					<SizedBox height={15} />
-					<Formik
-						initialValues={{ email: "" }}
-						onSubmit={async (values, { setErrors }) => {
-							const response = await forgotPassword(values);
-							if (response?.error) {
-								await handleGraphqlError(response, setAlert);
-								if (response.data?.forgotPassword === false) {
-									setAlert("error: something went wrong, try again");
-								}
-							} else if (response.data?.forgotPassword) {
-								await handleGraphqlSuccess(
-									response,
-									setAlert,
-									"forgotPassword",
-									"we will send you an email if the email is valid"
-								);
-								router.push("/");
-							}
-						}}
-					>
-						{({ isSubmitting }) => (
-							<Form>
-								<InputField
-									name="email"
-									placeholder="email"
-									label="Email"
-									colorConfig={colorConfig}
-								/>
-								<Button
-									mt={5}
-									type="submit"
-									bgColor={colorConfig.bgColor[colorMode]}
-									color="white"
-									isLoading={isSubmitting}
+					<Box w="100%" px={5}>
+						<SizedHeading variant="xl" title="Forgot password" />
+						<Box h={5} />
+						<Text fontSize="lg">
+							enter your email to request for a password change
+						</Text>
+						{alert === "" ? null : (
+							<Flex my={4} w="100%" justifyContent="center">
+								<Alert
+									maxW={{ base: "450px", md: "600px" }}
+									status={
+										alert.split(":")[0].includes("error") ? "error" : "success"
+									}
 								>
-									Request for password reset
-								</Button>
-							</Form>
+									<AlertIcon />
+									<AlertTitle mr={2}>{alert}</AlertTitle>
+								</Alert>
+							</Flex>
 						)}
-					</Formik>
+						<SizedBox height={15} />
+						<Formik
+							validationSchema={ForgetPasswordValidationSchema}
+							initialValues={{ email: "" }}
+							onSubmit={async (values) => {
+								const response = await forgotPassword(values);
+								if (response?.error) {
+									await handleGraphqlError(response, setAlert);
+									if (response.data?.forgotPassword === false) {
+										setAlert("error: something went wrong, try again");
+									}
+								} else if (response.data?.forgotPassword) {
+									await handleGraphqlSuccess(
+										response,
+										setAlert,
+										"forgotPassword",
+										"we will send you an email if the email is valid"
+									);
+									router.push("/");
+								}
+							}}
+						>
+							{({ isSubmitting }) => (
+								<Form>
+									<InputField
+										name="email"
+										placeholder="email"
+										label="Email"
+										colorConfig={colorConfig}
+									/>
+									<Button
+										mt={5}
+										type="submit"
+										bgColor={colorConfig.bgColor[colorMode]}
+										color="white"
+										isLoading={isSubmitting}
+									>
+										Request for password reset
+									</Button>
+								</Form>
+							)}
+						</Formik>
+					</Box>
 				</ResponsiveWrapper>
 			</Main>
 		</>
