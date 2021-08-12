0a140699cc9464a2c9e585eda128d38b3f456d63
Low Siew Kam Honey Nicole <winston_lim1@hotmail.com>
2021-08-12 22:31:19 +0800

    Create individual type file for Category

---
+++ src/types/Category.ts
@@ -0,0 +1,14 @@
+export enum Category {
+	GENERAL = "general",
+	NOTE_TAKING = "note-taking",
+	TIME_SAVER = "time-saver",
+	TIME_MANAGEMENT = "time-management",
+	HEALTH = "health",
+	PLANNING = "planning",
+	EDUCATION = "education",
+	UNIVERSITY = "university",
+	FINANCE = "finance",
+	TECHNOLOGY = "technology",
+	FASHION = "fashion",
+	OTHERS = "others",
+}
