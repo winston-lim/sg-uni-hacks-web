1d357f149a9bc185dd8bcb7f30c7b02a4cf22797
Low Siew Kam Honey Nicole <winston_lim1@hotmail.com>
2021-08-12 22:29:34 +0800

    Refactor useIsAuth hook

--- src/utils/useIsAuth.ts
+++ src/utils/useIsAuth.ts
@@ -6,6 +6,8 @@ export const useIsAuth = () => {
 	const router = useRouter();
 	const [{ data, fetching }] = useCurrentUserQuery();
 	useEffect(() => {
+		//if user is not authenticated redirect to login page with ?next= current path
+		//in the login page, onSubmit checks if ?next= exists, and if so routes user to where he was trying to reach
 		if (!data?.currentUser && !fetching) {
 			router.replace("/login?next=" + router.pathname);
 		}
@@ -13,4 +15,5 @@ export const useIsAuth = () => {
 	if (data?.currentUser && !fetching) {
 		return data.currentUser;
 	}
+	return;
 };
