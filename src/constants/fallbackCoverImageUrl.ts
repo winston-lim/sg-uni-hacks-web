ecf9f79d99b2259253efe0403859c26f8dc947ea
Low Siew Kam Honey Nicole <winston_lim1@hotmail.com>
2021-08-13 09:12:10 +0800

    Add defaultEditorData const

---
+++ src/constants/fallbackCoverImageUrl.ts
@@ -0,0 +1,2 @@
+export const fallbackCoverImageUrl =
+	"https://images.unsplash.com/photo-1608408843596-b3119736057c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2095&q=80";
